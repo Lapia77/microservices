c17efc692169a5bda3ad2f38cc1947010efd551d
Opportun LAPIA <opportunlapia@outlook.fr>
2024-09-22 23:04:41 +0100

    docker-compose

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductRequest(string name,
+                                   List<string> categories,
+                                   string description,
+                                   string imageFile,
+                                   decimal price);
+
+public record CreateProductResponse(Guid ProductId);
+public class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app) 
+    {
+        app.MapPost("/products",async(CreateProductRequest request, ISender sender) =>
+        {
+            var command= request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response= result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/Products/{response.ProductId}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product"); 
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string name,
+                                   List<string> categories, 
+                                   string description,
+                                   string imageFile,
+                                   decimal price ) : ICommand<CreateProductResult>;
+public record CreateProductResult( Guid productId );
+internal class CreateProductHandler(IDocumentSession _session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Business logic to create product
+        Product product = new Product()
+        {
+            Name = command.name,
+            Categories = command.categories,
+            Description = command.description,
+            ImageFile = command.imageFile,
+            Price = command.price
+        };
+
+        _session.Store( product );
+        await _session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+
+}
